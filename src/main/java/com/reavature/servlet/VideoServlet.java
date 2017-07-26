8277594c2975a8a600977570a39ab78b636ed3e6
Dc Liao <dcliao92@gmail.com>
2017-07-26 16:26:32 -0400

    add servlets

---
+++ src/main/java/com/reavature/servlet/VideoServlet.java
@@ -0,0 +1,28 @@
+package com.reavature.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class VideoServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -199241741419833109L;
+
+	@Override 
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+		throws ServletException, IOException{
+		//TODO get videos
+	}
+	
+	@Override 
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+		throws ServletException, IOException{
+		//TODO save video to s3, db
+	}
+}
